feebf25cf31615eff83c444064d9825c7da0f231
nacho-1 <56204692+nacho-1@users.noreply.github.com>
2026-05-05 20:13:16 -0300

    add typst-preview plugin
    Had to enable custom plugins on the main lua as well.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'chomosuke/typst-preview.nvim',
+    ft = 'typst',
+    version = '1.*',
+    opts = {},
+  },
+}
